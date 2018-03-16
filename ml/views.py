dda1118d186788adccc2232a9d0570a9bb3bfcaf
abar31 <abar@hvl.no>
2018-03-16 17:36:16 +0100

    System trains and evaluate correctly, results explained by using LIME, only by text

--- ml/views.py
+++ ml/views.py
@@ -10,12 +10,14 @@ from io import TextIOWrapper
 import pandas
 from .models import Dataset, Label
 from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import numpy
-
-
-
-
+from sklearn.metrics import mean_squared_error
+import lime
+import lime.lime_tabular
+from sklearn.svm import SVR
+from sklearn.svm import SVC
 #index page
 def index(request):
     return render(request, 'ml/index.html', locals())
@@ -35,6 +37,9 @@ def results(request):
         samples = reader.values
         nsamples= len(reader)*nlabels
         
+        #raw data into session
+        request.session['dstfile'] = reader.to_json()
+
         #save dataset in db
         dst = Dataset(dataset_name = name, labels_number = nlabels, samples_number = nsamples)
         dst.save()
@@ -42,7 +47,7 @@ def results(request):
         label_ids = []
 
         #save labels in db
-        for label in labels: 
+        for label in labels:
             smpls = list(reader[label].values)
             lbl = Label(dataset = dst, label_name = label, samples = smpls)
             lbl.save()
@@ -70,19 +75,19 @@ def confirm(request):
                 lbl = Label.objects.get(id=ids)
                 lbl.use_as = 'IG' 
                 lbl.save()
-                ignored.append(lbl.id)
-
+                ignored.append(lbl.label_name)
+        #ignored into session        
         request.session['ignored'] = ignored
         #targeted label        
         ids = request.POST.get('target')
         lbl = Label.objects.get(id=ids)
         lbl.use_as = 'OU' 
         lbl.save()
-        request.session['target'] = lbl.id
+        request.session['target'] = lbl.label_name
         #get percentages
         percs = request.POST.get('percentages')
         dst = Dataset.objects.get(id = dataset_id)
-
+        #check percentages not default
         if ('na' not in percs) and ('60' not in percs):
             percs = percs.split("-")
             dst.train_percent = int(percs[0])
@@ -97,34 +102,38 @@ def confirm(request):
 
 #train page
 def train(request):
-    dataset_id = request.session['dataset']
-    label_ids = request.session['labels']
-    X = []
+
     if request.POST:
-        X = Label.objects.get(id = request.session['target']).samples
-        y = []
-        for ids in label_ids:
-            if ids != request.session['target']:
-                lbl = Label.objects.get(id=ids)
-                y.append(lbl.samples)
+        total = pandas.read_json(request.session['dstfile'], orient='records')
+        X = total[[request.session['target']]]
+        sub = total.drop(request.session['target'], 1)
+        if request.session['ignored']:
+            for ig in request.session['ignored']:
+                sub = sub.drop(ig, 1)
+        y = sub        
 
-    #x, x_test, y, y_test = train_test_split(xtrain,labels,test_size=0.2,train_size=0.8)
-    #x_train, x_cv, y_train, y_cv = train_test_split(x,y,test_size = 0.25,train_size =0.75)
-
-    a = numpy.array(X)
-    b = numpy.array(y)
-    dst = Dataset.objects.get(id = dataset_id)
-    #a.reshape((int(dst.samples_number/len(label_ids)),1))
-    a = a.reshape((1599, 1))
-    b = b. reshape((1599, 11))
-    print(a)
-    print(b)
-    #TODO: double array for numbers and text, put this into numbers
-    X_train, X_test, y_train, y_test = train_test_split(a, b, test_size = (Dataset.objects.get(id = dataset_id).test_percent)/100)
-    #model = LinearSVC()
-    #model.fit(X_train, y_train)
-    #predict_data = model.predict(y_test)
-    #print (predict_data)
-
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = ((Dataset.objects.get(id = request.session['dataset']).test_percent)/100)*2)
+    model = SVC(probability=True)
+    #model = LogisticRegression()
+    #model = SVR()
+    model.fit(y_train, X_train)
+    predict_data = model.predict(y_test)
+    #print(predict_data)
+    #predict_data = model.predict(numpy.array([[10.8, 0.45, 0.33, 2.5, 0.099, 20.0, 38.0, 0.99818, 3.24, 0.71, 10.8]]))
+    #acc = mean_squared_error(X_test,predict_data)
+    #print (acc*100)
+    features=[]
+    label_ids = request.session['labels'] 
+    for ids in label_ids:
+       lbl = Label.objects.get(id=ids)  
+       features.append(lbl.label_name)
+    #print (features)
+    #predict = lambda x: predict_data.astype(float)     
+    explainer = lime.lime_tabular.LimeTabularExplainer(y_test, feature_names=features, class_names=request.session['target'],discretize_continuous=False)
+    i = numpy.random.randint(0, len(y_test))
+    print(y_test.iloc[i])
+    print(model.predict([y_test.iloc[i]]))
+    exp = explainer.explain_instance(y_test.iloc[i], model.predict_proba, num_features=11)
+    print(exp.as_list())
 
     return render(request, 'ml/train.html', locals())
