0e67745c6a90ba600078b083401df3daa61c3d8d
abar31 <abar@hvl.no>
2018-03-19 18:08:43 +0100

    Accuracy improved by adding extra column, train.html WIP

--- ml/views.py
+++ ml/views.py
@@ -18,6 +18,18 @@ import lime
 import lime.lime_tabular
 from sklearn.svm import SVR
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+
+
+
+
 #index page
 def index(request):
     return render(request, 'ml/index.html', locals())
@@ -27,8 +39,21 @@ def results(request):
     #reads CSV file sent from index
     if request.POST and request.FILES:
         name = request.FILES['csv_file'].name
-        csvfile = TextIOWrapper(request.FILES['csv_file'].file, encoding='utf-8')
-        reader = pandas.read_csv(csvfile, delimiter = ',')
+        file = request.FILES['csv_file']
+        #csvfile = TextIOWrapper(request.FILES['csv_file'].file, encoding='utf-8')
+
+        reader = pandas.read_csv(file)
+########################################################
+        reviews = []
+        for i in reader['quality']:
+            if i >= 3 and i <= 5:
+                reviews.append('1')
+            elif i >= 6 and i <= 6:
+                reviews.append('2')
+            elif i >= 7 and i <= 8:
+                reviews.append('3')
+        reader['Reviews'] = reviews
+########################################################
         #get first row as labels
         labels = list(reader)
         nlabels = len(labels)
@@ -36,9 +61,9 @@ def results(request):
         #rest of rows as values
         samples = reader.values
         nsamples= len(reader)*nlabels
-        
+  
         #raw data into session
-        request.session['dstfile'] = reader.to_json()
+        request.session['dstfile'] = reader.to_json(orient ='records')
 
         #save dataset in db
         dst = Dataset(dataset_name = name, labels_number = nlabels, samples_number = nsamples)
@@ -104,36 +129,56 @@ def confirm(request):
 def train(request):
 
     if request.POST:
+
         total = pandas.read_json(request.session['dstfile'], orient='records')
-        X = total[[request.session['target']]]
+
+        y = total[[request.session['target']]]
         sub = total.drop(request.session['target'], 1)
         if request.session['ignored']:
             for ig in request.session['ignored']:
                 sub = sub.drop(ig, 1)
-        y = sub        
+        x = sub
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = ((Dataset.objects.get(id = request.session['dataset']).test_percent)/100)*2)
-    model = SVC(probability=True)
-    #model = LogisticRegression()
-    #model = SVR()
-    model.fit(y_train, X_train)
-    predict_data = model.predict(y_test)
-    #print(predict_data)
-    #predict_data = model.predict(numpy.array([[10.8, 0.45, 0.33, 2.5, 0.099, 20.0, 38.0, 0.99818, 3.24, 0.71, 10.8]]))
-    #acc = mean_squared_error(X_test,predict_data)
-    #print (acc*100)
-    features=[]
-    label_ids = request.session['labels'] 
-    for ids in label_ids:
-       lbl = Label.objects.get(id=ids)  
-       features.append(lbl.label_name)
-    #print (features)
-    #predict = lambda x: predict_data.astype(float)     
-    explainer = lime.lime_tabular.LimeTabularExplainer(y_test, feature_names=features, class_names=request.session['target'],discretize_continuous=False)
-    i = numpy.random.randint(0, len(y_test))
-    print(y_test.iloc[i])
-    print(model.predict([y_test.iloc[i]]))
-    exp = explainer.explain_instance(y_test.iloc[i], model.predict_proba, num_features=11)
-    print(exp.as_list())
+        sc = StandardScaler()
+        x = sc.fit_transform(x)
 
-    return render(request, 'ml/train.html', locals())
+
+        x2_train, x2_test, y2_train, y2_test = train_test_split(sub, y, test_size = ((Dataset.objects.get(id = request.session['dataset']).test_percent)/100))
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = ((Dataset.objects.get(id = request.session['dataset']).test_percent)/100))
+
+        model = SVC(probability = True)
+        model.fit(x_train, y_train) 
+
+        model2 = SVC(probability = True)
+        model2.fit(x2_train, y2_train) 
+        
+        
+        predict_data = model.predict(x_test)
+
+        #print confusion matrix and accuracy score
+        predict = accuracy_score(y_test, predict_data)*100
+        print(predict*100)
+        #print(X_test)
+        #print(predict_data)
+
+        #predict_data = model.predict(numpy.array([[8.1, 0.56, 0.28, 1.7, 0.368, 16.0, 56.0, 0.9968, 3.11, 1.28, 9.3]]))
+        print(x2_test)
+        # 8.1 0.56    0.28    1.7 0.368   16.0    56.0    0.9968  3.11    1.28    9.3 5
+        #acc = accuracy_score(y_test, predict_data)
+        #print (acc)
+
+        features = []
+        label_ids = request.session['labels'] 
+        for ids in label_ids:
+           lbl = Label.objects.get(id=ids)  
+           features.append(lbl.label_name)
+        explainer = lime.lime_tabular.LimeTabularExplainer(x2_test, feature_names=features, class_names=request.session['target'],discretize_continuous=False)
+
+        i = numpy.random.randint(0, len(x2_test))
+        exp = explainer.explain_instance(x2_test.iloc[i], model2.predict_proba, num_features=Dataset.objects.get(id = request.session['dataset']).labels_number-1)
+        print(exp.as_list())
+
+        context = {'accuracy': predict, 'predict_data': predict_data, 'test_data': x2_test.values, 'explanation': exp.as_list()}
+
+    return render(request, 'ml/train.html', context)
