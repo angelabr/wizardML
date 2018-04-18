95b4367460dafd26dfcfaf0b01e7fdb114593e36
abar31 <abar@hvl.no>
2018-04-18 16:40:51 +0200

    CSS and JS added. Index.html with UI

---
+++ ml/static/ml/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	//INDEX.HTML
+	$('#loading').hide();
+	$("#getstarted").click(function(event){
+		$('#index-text').fadeOut( "slow", function() {
+		});
+		$('#index-img').fadeOut( "slow", function() {
+			$('#index-form').fadeIn( "slow", function() {
+			});
+		});
+	});
+
+	$('#file').change(function() {
+		$('#file-form').submit();
+		$('#loading').show();
+	});
+});
